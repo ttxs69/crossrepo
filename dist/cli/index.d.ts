7d7ce48c18e432f13a118632ec08c02d64eed689
ttxs69 <ttxs69@users.noreply.github.com>
2026-03-11 07:59:47 +0800

    chore: add .gitignore and remove tracked node_modules/dist

--- dist/cli/index.d.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-/**
- * @fileoverview CrossRepo CLI
- * @module crossrepo/cli
- */
-export {};
-//# sourceMappingURL=index.d.ts.map
--- dist/types.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * @fileoverview Type definitions for CrossRepo
- * @module crossrepo/types
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=types.js.map
